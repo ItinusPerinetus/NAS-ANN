b6388c9855974471700fa56d14ba651e3194bb0d
Natchaphol Chumpanin <117467437+ItinusPerinetus@users.noreply.github.com>
2024-10-11 19:05:12 +0700

    Update model.py
    Make code looks more easy

--- model.py
+++ model.py
@@ -2,12 +2,13 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.optimizers import Adam
 
-def create_model(input_number, output_number, learning_rate, kernel_initializer, bias_initializer):
+def create_model(neurons, input_number = 10, output_number = 10, learning_rate = 0.001, kernel_initializer = 'glorot_uniform', bias_initializer = 'zeros'):
     inputs = Input(shape=(input_number,))
+    x = Dense(neurons)(inputs)
     outputs = Dense(output_number, activation='linear',
                     kernel_initializer=kernel_initializer,
                     bias_initializer=bias_initializer,
-                    use_bias=use_bias)(inputs)
+                    use_bias=use_bias)(x)
     opt = Adam(learning_rate=learning_rate)
     loss = 'mean_squared_error'
     model = Model(inputs, outputs)
